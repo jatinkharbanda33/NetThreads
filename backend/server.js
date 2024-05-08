0bec8292d1ab7c2c7bd3f13284d008a20e53e8a9
Jatin Kharbanda <76038276+jatinkharbanda33@users.noreply.github.com>
2024-05-09 00:26:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-/* import {v2 as cloudinary} from 'cloudinary'; */
+import {v2 as cloudinary} from 'cloudinary';
 import {connect} from './ConnectDB/connectToDb.js';
-/* import userRoutes from './Routes/userRoutes.js';
-import postRoutes from './Routes/postRoutes.js'; */
+import userRoutes from './Routes/userRoutes.js';
+import postRoutes from './Routes/postRoutes.js';
 const app=express();
 const port=process.env.PORT || 5000;
 
 connect();
-/* cloudinary.config({
+cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
- */
+
 //Middlewares
 app.use(express.json({ limit: "50mb" })); 
 app.use(express.urlencoded({ extended: true })); 
@@ -24,7 +24,7 @@ app.get("/",(req,res)=>{
 	return res.status(200).json({status:true,message:"Server Started"});
 });
 
-/* app.use("/api/users",userRoutes);
-app.use("/api/posts",postRoutes); */
+app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
 app.listen(port,()=>console.log(`Listening on port ${port}`)); 
 
