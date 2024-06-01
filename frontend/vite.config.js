d04abbb17f024f5321a69e22b65d9df9e905dfbd
Jatin Kharbanda <76038276+jatinkharbanda33@users.noreply.github.com>
2024-06-02 02:18:56 +0530

    Added proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       "/api":{
         target: "https://netthreads.jumpingcrab.com",
 				changeOrigin: true,
-				secure: false,
+				secure: true,
       }
     }
   }
